3d99947a6a2d5ad2bebc4810b4c73eabc44527b1
halil81 <ozdmr.h@gmail.com>
2021-12-14 11:05:43 +0300

    UserPostCard converted to StatelessWidget

--- lib/model/user_post.dart
+++ lib/model/user_post.dart
@@ -3,13 +3,15 @@ class UserPost {
   late final int? id;
   late final String? title;
   late final String? body;
+  bool? checked;
 
-  UserPost({this.userId, this.id, this.title, this.body});
+  UserPost({this.userId, this.id, this.title, this.body, this.checked});
 
   factory UserPost.fromJson(Map<String, dynamic> json) => UserPost(
         userId: json['userId'],
         id: json['id'],
         title: json['title'],
         body: json['body'],
+        checked: false,
       );
 }
--- lib/view/user_post_card.dart
+++ lib/view/user_post_card.dart
@@ -4,84 +4,42 @@ import 'package:flutter_bloc_template/bloc/generic_event.dart';
 import 'package:flutter_bloc_template/bloc/selection_bloc.dart';
 import 'package:flutter_bloc_template/bloc/selection_event.dart';
 
-class UserPostCard extends StatefulWidget {
+class UserPostCard extends StatelessWidget {
   final LoadedState state;
   final int index;
-  const UserPostCard({Key? key, required this.state, required this.index}) : super(key: key);
 
-  @override
-  _UserPostCardState createState() => _UserPostCardState();
-}
+  const UserPostCard({Key? key, required this.state, required this.index})
+      : super(key: key);
 
-class _UserPostCardState extends State<UserPostCard> {
   @override
   Widget build(BuildContext context) {
-    bool isChecked = false;
-    return StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
-      return Card(
-        clipBehavior: Clip.antiAlias,
-        child: Column(
-          children: [
-            ListTile(
-              //leading: const Icon(Icons.podcasts_outlined),
-              title: Text(widget.state.items![widget.index].title ??= 'Card Title'),
-              subtitle: Text(
-                widget.state.items![widget.index].body ??= 'Secondary Text',
-                style: TextStyle(color: Colors.black.withOpacity(0.6)),
-              ),
-              trailing: Checkbox(
-                value: isChecked,
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(state.items![index].title ??= 'Card Title'),
+            subtitle: Text(
+              state.items![index].body ??= 'Secondary Text',
+              style: TextStyle(color: Colors.black.withOpacity(0.6)),
+            ),
+            trailing:
+                BlocBuilder<SelectionBloc, int>(builder: (context, count) {
+              return Checkbox(
                 onChanged: (bool? value) {
-                  setState(() {
-                    isChecked = value!;
-                  });
-                  value == true ? context.read<SelectionBloc>().add(Increment()) : context.read<SelectionBloc>().add(Decrement());
+                  state.items![index].checked = value!;
+                  value == true
+                      ? context.read<SelectionBloc>().add(Increment())
+                      : context.read<SelectionBloc>().add(Decrement());
                 },
                 tristate: false,
+                value: state.items![index].checked,
                 activeColor: const Color(0xFF6200EE),
-              ),
-              //
-            ),
-          ],
-        ),
-      );
-    });
+              );
+            }),
+          ),
+        ],
+      ),
+    );
   }
 }
-
-//class UserPostCard extends StatelessWidget {
-//  final LoadedState state;
-//  final int index;
-//
-//  const UserPostCard({Key? key, required this.state, required this.index}) : super(key: key);
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    bool isChecked = false;
-//    return Card(
-//      clipBehavior: Clip.antiAlias,
-//      child: Column(
-//        children: [
-//          ListTile(
-//            //leading: const Icon(Icons.podcasts_outlined),
-//            title: Text(state.items![index].title ??= 'Card Title'),
-//            subtitle: Text(
-//              state.items![index].body ??= 'Secondary Text',
-//              style: TextStyle(color: Colors.black.withOpacity(0.6)),
-//            ),
-//            trailing: Checkbox(
-//              onChanged: (bool? value) {
-//                isChecked = value!;
-//                value == true ? context.read<SelectionBloc>().add(Increment()) : context.read<SelectionBloc>().add(Decrement());
-//              },
-//              tristate: false,
-//              value: isChecked,
-//              activeColor: const Color(0xFF6200EE),
-//            ),
-//            //
-//          ),
-//        ],
-//      ),
-//    );
-//  }
-//}
